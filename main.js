3910a0a241aa602ec37122f818465bc682380acb
Rhuan Carlos <dev.rhuancarlos@gmail.com>
2023-12-29 09:15:10 -0300

    teste

--- main.js
+++ main.js
@@ -1,50 +1,62 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupDestinos } from './obterDestinos.js'
-import { setupCounter } from './counter.js'
 
+let origensBox = document.querySelector('#origem');
+let destinoBox = document.querySelector('#destino');
+let plataforma = document.querySelector('#plataforma');
+let button = document.querySelector('#button')
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Proximo horário</h1>
-    <div>
-      <select id="origem"></select>
-    </div>
-    <div>
-      <select id="destino"></select>
-    </div>
-    <div>
-      <select id="horarios"></select>
-    </div>
-    <div>
-      <select id="plataforma"></select>
-    </div>
-    <button id="chamar">Chamar</button>
-  </div>
-`
+// window.speechSynthesis.addEventListener('')
 
-// setupCounter(document.querySelector('#counter'))
-
-const destinos = setupDestinos().sort();
-const origem = document.querySelector('#origem')
-const destino = document.querySelector('#destino')
-const chamar = document.querySelector('#chamar')
-
-const setupSelect = (element) => {
-  for (let item in destinos) {
-    const option = document.createElement('option')
-    option.setAttribute('value', destinos[item]);
-    option.innerText = destinos[item];
-    element.appendChild(option);
-  }
-}
-setupSelect(origem)
-setupSelect(destino)
-
-chamar.addEventListener('click', () => {
-  let ut = new SpeechSynthesisUtterance(`teste`);
-  window.speechSynthesis.speak(ut)
+button.addEventListener('click', () => {
+    let texto = `Atenção passageiros para o carro de ${origem.value} com destino ${destino.value}, o carro acaba de estacionar na plataforma ${plataforma.value}`
+    let ut = new SpeechSynthesisUtterance(texto);
+    window.speechSynthesis.speak(ut);
 })
+
+// Função assíncrona para obter dados de um arquivo JSON usando Fetch
+async function obterDados() {
+    try {
+        const resposta = await fetch('./teste.json');
+        if (!resposta.ok) {
+            throw new Error(`Erro ao carregar o JSON. Status: ${resposta.status}`);
+        }
+        lista = await resposta.json();
+    } catch (error) {
+        console.error('Erro ao obter dados:', error.message);
+    }
+}
+
+const obterValoresUnicos = (chave) => {
+    try {
+        // Utilize a lista diretamente, pois ela já é um array após a resolução da Promise
+        const valores = lista.map(item => item[chave]);
+        const valoresUnicos = [...new Set(valores)];
+
+        return JSON.stringify(valoresUnicos);
+    } catch (error) {
+        console.error('Erro ao processar o JSON:', error.message);
+    }
+}
+
+const montaSelect = (string, select) => {
+
+    const jsonDestinos = JSON.parse(string);
+    for (let i = 0; i < jsonDestinos.length; i++) {
+        console.log(jsonDestinos[i]);
+        const option = document.createElement("option")
+        option.setAttribute('value', jsonDestinos[i]);
+        option.innerText = jsonDestinos[i];
+        select.appendChild(option);
+
+    }
+}
+let lista = [];
+
+// Aguarde o carregamento completo da página antes de executar o código
+window.onload = async () => {
+    await obterDados();
+    const origens = obterValoresUnicos('Origem');
+    const destinos = obterValoresUnicos('Destino');
+    montaSelect(origens, origensBox);
+    montaSelect(destinos, destinoBox);
+
+};
